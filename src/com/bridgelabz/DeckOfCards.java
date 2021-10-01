6f53c67af97e2f01fddcd1513fa018c45250daca
vanna2020 <vannasingh1999@gmail.com>
2021-09-30 19:58:01 -0700

    Welcome To Deck Of Cards

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+/*
+DeckOfCards class represents a deck of playing cards
+ */
+        public class DeckOfCards {
+
+                  public static void main(String[] args) {
+                  System.out.println("welcome to Deck of Cards");
+
+    }
+}
